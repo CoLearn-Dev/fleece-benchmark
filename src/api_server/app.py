a1513e54931eadde06f82ac7991f8c2bf6d6d41d
walotta <wzj2001@sjtu.edu.cn>
2023-11-07 00:07:33 +0000

    add support for https of api_server

--- src/api_server/app.py
+++ src/api_server/app.py
@@ -130,4 +130,4 @@ def trace_tps(id: str, model: str, sample_len: int = 5):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile='key.pem', ssl_certfile='fullchain.pem')
